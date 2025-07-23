aaa3ed7ce098a03783edfd84744ffcd7099e5345
Cirooochen <cirochen0406@gmail.com>
2025-07-23 11:30:38 +0200

    added Render related code

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import notesRouter from "./routes/notesRouter.js";
 import userRouter from "./routes/userRouter.js";
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.use(express.json()); //middleware, to convert any javascript into json format.
 
@@ -14,5 +14,7 @@ app.use("/users", userRouter);
 app.use("/notes", notesRouter);
 
 app.listen(8080, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  process.env.NODE_ENV === "production"
+    ? console.log(`Server is running on production`)
+    : console.log(`Server is running on http://localhost:${port}`);
 });
